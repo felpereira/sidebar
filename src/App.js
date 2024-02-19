11687d70b9dbdb6e78736918e1aa4cbdbd6f3269
cb.felipe <cb.felipe@gmail.com>
2024-02-18 22:42:44 -0300

    Primeiras impressoes do novo sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 import { ReactComponent as Logo } from './menusButton.svg';
+import { ReactComponent as Logotipo } from './logotipo.svg';
+import { ReactComponent as Home } from './home.svg';
+import { ReactComponent as Post } from './posticon.svg';
 
 function App() {
   const [exibir, setExibir] = useState(false);
   const style = exibir ? 'exibirDireitaProCima' : '';
+  const [selecionado, setSelecionado] = useState(1);
 
   useEffect(() => {
     let posicaoX = 0;
@@ -18,7 +22,7 @@ function App() {
 
         if (posicaoX !== 0 && posicaoX + 50 < currentX) {
           const catetoAdjacente = event.touches[0].clientX - posicaoX
-          const { posicaoYMax, posicaoYMIn } = CalcularPosicaoMaxEMinY(catetoAdjacente);
+          const { posicaoYMax, posicaoYMIn } = this.CalcularPosicaoMaxEMinY(catetoAdjacente);
           if ((currentY < posicaoYMax && currentY > posicaoYMIn)) {
             setExibir(true);
           }
@@ -26,7 +30,7 @@ function App() {
 
         if (posicaoX !== 0 && posicaoX - 50 > currentX) {
           const catetoAdjacente = posicaoX - event.touches[0].clientX;
-          const { posicaoYMax, posicaoYMIn } = CalcularPosicaoMaxEMinY(catetoAdjacente);
+          const { posicaoYMax, posicaoYMIn } = this.CalcularPosicaoMaxEMinY(catetoAdjacente);
 
           if ((currentY < posicaoYMax && currentY > posicaoYMIn)) {
             setExibir(false);
@@ -41,44 +45,77 @@ function App() {
       function calcularPosicaoMaxEMinY(catetoAdjacente) {
         // Convertendo o ângulo para radianos
         const anguloRadianos = (Math.PI / 180) * 15;
-        
+
         // Calculando o cateto oposto com base no ângulo
         const catetoOposto = Math.tan(anguloRadianos) * catetoAdjacente;
-        
+
         // Calculando as posições máxima e mínima ao longo do eixo Y
         const posicaoYMax = posicaoY + catetoOposto;
         const posicaoYMin = posicaoY - catetoOposto;
-        
+
         // Retornando as posições calculadas
         return { posicaoYMax, posicaoYMin };
       }
 
-    const handleWindowTouchStart = event => {
-      isTouching = true;
-      posicaoX = event.touches[0].clientX;
-      posicaoY = event.touches[0].clientY;
-    };
+      const handleWindowTouchStart = event => {
+        isTouching = true;
+        posicaoX = event.touches[0].clientX;
+        posicaoY = event.touches[0].clientY;
+      };
 
-    const handleWindowTouchEnd = () => {
-      isTouching = false;
-    };
+      const handleWindowTouchEnd = () => {
+        isTouching = false;
+      };
 
-    window.addEventListener('touchstart', handleWindowTouchStart);
-    window.addEventListener('touchmove', handleWindowTouchMove);
-    window.addEventListener('touchend', handleWindowTouchEnd);
+      window.addEventListener('touchstart', handleWindowTouchStart);
+      window.addEventListener('touchmove', handleWindowTouchMove);
+      window.addEventListener('touchend', handleWindowTouchEnd);
 
-    return () => {
-      window.removeEventListener('touchstart', handleWindowTouchStart);
-      window.removeEventListener('touchmove', handleWindowTouchMove);
-      window.removeEventListener('touchend', handleWindowTouchEnd);
-    };
+      return () => {
+        window.removeEventListener('touchstart', handleWindowTouchStart);
+        window.removeEventListener('touchmove', handleWindowTouchMove);
+        window.removeEventListener('touchend', handleWindowTouchEnd);
+      };
+    }
   }, []);
+
+  const cliqueDiv = (e, selecionado) => {
+    e.preventDefault();
+    setSelecionado(selecionado)
+
+    return null;
+  }
 
   return (
     <div className="conteudo">
-      <div className={`direita ${style}`} ></div>
-      <div className='esquerda'>
+      <div className={`direita ${style}`} >
+        {/* Brand + Nav List */}
+        <div className={`brand-navlist`}>
+          {/* Brand */}
+          <div className={`brand`}>
+            <Logotipo width={40} height={40} />
+            <div className={`brand-title`}>
+              Appli
+            </div>
+          </div>
+          {/* Nav List */}
+          <button className={`nav-icons selecionado`} onKeyDown={e => cliqueDiv(e, 1)}>
+            <Home width={40} height={40} />
+            <div className={`nav-text selecionado`}>
+              Dashboard
+            </div>
+          </button>
 
+          <button className={`nav-icons`} onKeyDown={e => cliqueDiv(e, 2)}>
+            <Post width={40} height={40} />
+            <div className={`nav-text`}>
+              Posts
+            </div>
+          </button>
+
+        </div>
+      </div>
+      <div className='esquerda'>
         <div
           style={{
             display: 'inline-flex',
