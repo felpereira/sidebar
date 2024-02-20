66d65ca17169777519f517a7e2cdb633df5145b4
cb.felipe <cb.felipe@gmail.com>
2024-02-19 21:42:44 -0300

    sideBar aberto pronto

--- src/App.js
+++ src/App.js
@@ -2,13 +2,32 @@ import { useState, useEffect } from 'react';
 import './App.css';
 import { ReactComponent as Logo } from './menusButton.svg';
 import { ReactComponent as Logotipo } from './logotipo.svg';
-import { ReactComponent as Home } from './home.svg';
-import { ReactComponent as Post } from './posticon.svg';
+import { ReactComponent as Home } from './svg/Home.svg';
+import { ReactComponent as EditSquare } from './svg/EditSquare.svg';
+import { ReactComponent as Image } from './svg/Image.svg';
+import { ReactComponent as Document } from './svg/Document.svg';
+import { ReactComponent as Chat } from './svg/Chat.svg';
+import { ReactComponent as Category } from './svg/Category.svg';
+import { ReactComponent as Bag } from './svg/Bag.svg';
+import { ReactComponent as User } from './svg/User.svg';
+import { ReactComponent as Setting } from './svg/Setting.svg';
+import { ReactComponent as Filter } from './svg/Filter.svg';
+import { ReactComponent as Moon } from './svg/moon.svg';
+import { ReactComponent as Toogle } from './svg/Toogle.svg';
+
+import { ReactComponent as Logout } from './svg/Logout.svg';
+
+
+
+import { NavButton } from './NavButton';
+
 
 function App() {
   const [exibir, setExibir] = useState(false);
   const style = exibir ? 'exibirDireitaProCima' : '';
   const [selecionado, setSelecionado] = useState(1);
+
+  console.log(selecionado)
 
   useEffect(() => {
     let posicaoX = 0;
@@ -86,6 +105,10 @@ function App() {
     return null;
   }
 
+  const definirSelecionado = (id) => {
+    setSelecionado(id);
+  }
+
   return (
     <div className="conteudo">
       <div className={`direita ${style}`} >
@@ -99,20 +122,34 @@ function App() {
             </div>
           </div>
           {/* Nav List */}
-          <button className={`nav-icons selecionado`} onKeyDown={e => cliqueDiv(e, 1)}>
-            <Home width={40} height={40} />
-            <div className={`nav-text selecionado`}>
-              Dashboard
+          <div>
+            <NavButton Icon={<Home width={30} height={30} />}
+              texto="Dashboard" id={1} selecionado={selecionado === 1} definirSelecionado={definirSelecionado} />
+            <NavButton Icon={<EditSquare width={30} height={30} />}
+              texto="Posts" id={2} selecionado={selecionado === 2} definirSelecionado={definirSelecionado} />
+            <NavButton Icon={<Image width={30} height={30} />}
+              texto="Media" id={3} selecionado={selecionado === 3} definirSelecionado={definirSelecionado} />
+            <NavButton Icon={<Document width={30} height={30} />}
+              texto="Pages" id={4} selecionado={selecionado === 4} definirSelecionado={definirSelecionado} />
+            <NavButton Icon={<Chat width={30} height={30} />}
+              texto="Commets" id={5} selecionado={selecionado === 5} definirSelecionado={definirSelecionado} />
+            <NavButton Icon={<Category width={30} height={30} />}
+              texto="Appearance" id={6} selecionado={selecionado === 6} definirSelecionado={definirSelecionado} />
+            <NavButton Icon={<Bag width={30} height={30} />}
+              texto="Plugins" id={7} selecionado={selecionado === 7} definirSelecionado={definirSelecionado} />
+            <NavButton Icon={<User width={30} height={30} />}
+              texto="Users" id={8} selecionado={selecionado === 8} definirSelecionado={definirSelecionado} />
+            <NavButton Icon={<Setting width={30} height={30} />}
+              texto="Settings" id={9} selecionado={selecionado === 9} definirSelecionado={definirSelecionado} />
+            <NavButton Icon={<Filter width={30} height={30} />}
+              texto="Tools" id={10} selecionado={selecionado === 10} definirSelecionado={definirSelecionado} />
+          </div>
+          <div className={`botton-nav`}>
+            <div className={`darkmode`}>
+              <Moon width={33} height={33} />Dark Mode<Toogle width={50} height={50} />
             </div>
-          </button>
-
-          <button className={`nav-icons`} onKeyDown={e => cliqueDiv(e, 2)}>
-            <Post width={40} height={40} />
-            <div className={`nav-text`}>
-              Posts
-            </div>
-          </button>
-
+            <button className={`buttonLogout`}>  <Logout width={33} height={33} /><div className={`buttonText`}>Logout</div></button>
+          </div>
         </div>
       </div>
       <div className='esquerda'>
